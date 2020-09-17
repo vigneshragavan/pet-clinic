52ec9301d6f0d7d81297f672eceaffdf7b930579
vignesh-ragavan <vigneshr.vtalent@gmail.com>
2020-09-17 09:50:58 -0700

    refractoring to common crud srvice.closes #5

--- pet-clinic-data/src/main/java/com/vignesh/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/vignesh/sfgpetclinic/services/OwnerService.java
@@ -2,12 +2,8 @@ package com.vignesh.sfgpetclinic.services;
 
 import com.vignesh.sfgpetclinic.model.Owner;
 
-import java.util.Set;
+public interface OwnerService extends CrudService<Owner ,Long> {
 
-public interface OwnerService {
+    Owner findByLastName(String lastName);
 
-    Owner finfByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/vignesh/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/vignesh/sfgpetclinic/services/PetService.java
@@ -2,10 +2,6 @@ package com.vignesh.sfgpetclinic.services;
 
 import com.vignesh.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet,Long> {
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
