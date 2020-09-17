57b6309dd81d44114d1fafaa8446b276a3c719e4
vignesh-ragavan <vigneshr.vtalent@gmail.com>
2020-09-17 10:31:54 -0700

    implement map based service.closes #6

---
+++ pet-clinic-data/src/main/java/com/vignesh/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package com.vignesh.sfgpetclinic.services.map;
+
+import com.vignesh.sfgpetclinic.model.Pet;
+import com.vignesh.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet,Long> implements CrudService<Pet,Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+  super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(),object
+        );
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
