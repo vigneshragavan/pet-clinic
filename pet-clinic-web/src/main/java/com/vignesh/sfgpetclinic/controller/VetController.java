4071cabf260a56b21821f8fb190a078a475ee357
vignesh-ragavan <vigneshr.vtalent@gmail.com>
2020-09-20 06:23:27 -0700

    loaded list of vets and updated controller.closes #12

--- pet-clinic-web/src/main/java/com/vignesh/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/vignesh/sfgpetclinic/controller/VetController.java
@@ -1,13 +1,24 @@
 package com.vignesh.sfgpetclinic.controller;
 
+import com.vignesh.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index"})
-    public String  ListVets(){
+    public String  ListVets(Model model){
+
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
+
 }
 
