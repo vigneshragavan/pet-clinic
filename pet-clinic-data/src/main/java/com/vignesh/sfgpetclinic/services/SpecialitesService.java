fe8e962d8a999c145d7957bda063c57dcb9d9d0d
vignesh-ragavan <vigneshr.vtalent@gmail.com>
2020-09-21 09:24:04 -0700

    added speciality map service.closes #25

---
+++ pet-clinic-data/src/main/java/com/vignesh/sfgpetclinic/services/SpecialitesService.java
@@ -0,0 +1,6 @@
+package com.vignesh.sfgpetclinic.services;
+
+import com.vignesh.sfgpetclinic.model.Speciality;
+
+public interface SpecialitesService extends CrudService<Speciality,Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/vignesh/sfgpetclinic/services/map/SpecialitiesMapService.java
@@ -0,0 +1,35 @@
+package com.vignesh.sfgpetclinic.services.map;
+
+import com.vignesh.sfgpetclinic.model.Speciality;
+import com.vignesh.sfgpetclinic.services.SpecialitesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialitiesMapService extends  AbstractMapService<Speciality,Long> implements SpecialitesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+             super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object );
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
